3dba01cb743179c162b10b32b82901cd80743230
Pieter Kokx <pieter@kokx.nl>
2018-12-30 14:45:49 +0100

    Added wordlist for vouchers

--- portal/database/factories/VoucherFactory.php
+++ portal/database/factories/VoucherFactory.php
@@ -1,8 +1,10 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Faker\Gepwnage;
 
 $factory->define(App\Voucher::class, function (Faker $faker) {
+    $faker->addProvider(new Gepwnage($faker));
     return [
         'key' => $faker->words(4, true),
     ];
