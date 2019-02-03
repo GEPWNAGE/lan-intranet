0309e7196e16b492fc9ae3a17cd55a61befb23a3
Koen Klaren <koen.klaren@gmail.com>
2019-02-03 16:07:14 +0100

    Render time + day of the week in a Panel

--- beamer/src/panels/time/TimePanel.tsx
+++ beamer/src/panels/time/TimePanel.tsx
@@ -1,16 +1,27 @@
 import * as React from 'react';
 
+import Panel from '../Panel';
 import useTime from './useTime';
 
 export default function TimePanel() {
     const [time] = useTime();
 
     return (
-        <time>
-            {time.toLocaleString('nl-NL', {
-                hour: '2-digit',
-                minute: '2-digit',
-            })}
-        </time>
+        <Panel>
+            <div className="TimePanel">
+                <div className="TimePanel__time">
+                    {/* nl-NL to ensure 24 hour time formatting */}
+                    {time.toLocaleString('nl-NL', {
+                        hour: '2-digit',
+                        minute: '2-digit',
+                    })}
+                </div>
+                <div className="TimePanel__date">
+                    {time.toLocaleString('en-US', {
+                        weekday: 'long',
+                    })}
+                </div>
+            </div>
+        </Panel>
     );
 }
