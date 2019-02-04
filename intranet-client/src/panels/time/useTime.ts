1360713b0b1380a901fbaf95f5285a2ae8bb2bd2
Koen Klaren <koen.klaren@gmail.com>
2019-02-04 17:34:36 +0100

    Extract useInterval based on post on Overreacted

--- intranet-client/src/panels/time/useTime.ts
+++ intranet-client/src/panels/time/useTime.ts
@@ -1,17 +1,34 @@
 import * as React from 'react';
 
-export default function useTime(refreshRate = 1000): [Date] {
+// Taken from https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+export function useInterval(callback: () => void, delay: number | null) {
+    const savedCallback = React.useRef<typeof callback | null>(null);
+
+    // Remember the latest callback
+    React.useEffect(() => {
+        savedCallback.current = callback;
+    });
+
+    // Set up the interval
+    React.useEffect(() => {
+        function tick() {
+            if (savedCallback.current) {
+                savedCallback.current();
+            }
+        }
+        if (delay !== null) {
+            const id = setInterval(tick, delay);
+            return () => clearInterval(id);
+        }
+    }, [delay]);
+}
+
+export default function useTime(delay = 1000): [Date] {
     const [time, setTime] = React.useState(new Date());
 
-    React.useEffect(() => {
-        const handle = setInterval(() => {
-            setTime(new Date());
-        }, refreshRate);
-
-        return () => {
-            clearInterval(handle);
-        };
-    }, [refreshRate]);
+    useInterval(() => {
+        setTime(new Date());
+    }, delay);
 
     return [time];
 }
