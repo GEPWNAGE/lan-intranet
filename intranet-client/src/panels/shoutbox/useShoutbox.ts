4e67ede4e06081125d3b07b105655e04c3d440f5
Koen Klaren <koen.klaren@gmail.com>
2019-02-05 20:28:03 +0100

    Fetch existing messages when Shoutbox is mounted

--- intranet-client/src/panels/shoutbox/useShoutbox.ts
+++ intranet-client/src/panels/shoutbox/useShoutbox.ts
@@ -9,18 +9,81 @@ interface Message {
     time: Date;
 }
 
-function reducer(state: Message[], message: Message): Message[] {
-    return [...state, message];
+interface ShoutboxState {
+    ids: number[];
+    messagesById: { [id: number]: Message };
+}
+
+type ShoutboxAction = { type: 'RECEIVE_MESSAGES'; messages: Message[] };
+
+const initialState: ShoutboxState = {
+    ids: [],
+    messagesById: {},
+};
+
+function reducer(state: ShoutboxState, action: ShoutboxAction): ShoutboxState {
+    switch (action.type) {
+        case 'RECEIVE_MESSAGES':
+            // Normalize the result
+            const newIds = action.messages.map((msg) => msg.id);
+            const newMessagesById = action.messages.reduce<{
+                [id: number]: Message;
+            }>((obj, msg) => {
+                obj[msg.id] = msg;
+                return obj;
+            }, {});
+
+            // Combine with the existing messages
+            const ids = [...state.ids, ...newIds];
+            const messagesById = { ...state.messagesById, ...newMessagesById };
+
+            // Sort messages by time
+            ids.sort(
+                (a, b) =>
+                    messagesById[a].time.getTime() -
+                    messagesById[b].time.getTime(),
+            );
+
+            return {
+                ids: ids,
+                messagesById,
+            };
+        default:
+            return state;
+    }
+}
+
+function convertApiMessage(msg: any): Message {
+    return {
+        ...msg,
+        time: new Date(msg.time),
+    };
 }
 
 export default function useShoutbox(): [Message[]] {
     const socket = useSocket();
 
-    const [messages, dispatch] = useReducer(reducer, []);
+    const [state, dispatch] = useReducer(reducer, initialState);
 
+    // Initial request to get existing messages
     useEffect(() => {
-        function callback(message: Message) {
-            dispatch(message);
+        fetch('http://localhost:3030/api/shoutbox')
+            .then((res) => res.json())
+            .then((json) =>
+                dispatch({
+                    type: 'RECEIVE_MESSAGES',
+                    messages: json.messages.map(convertApiMessage),
+                }),
+            );
+    }, []);
+
+    // Receive new messages via socket
+    useEffect(() => {
+        function callback(msg: any) {
+            dispatch({
+                type: 'RECEIVE_MESSAGES',
+                messages: [convertApiMessage(msg)],
+            });
         }
 
         if (socket !== null) {
@@ -29,5 +92,5 @@ export default function useShoutbox(): [Message[]] {
         }
     }, [socket]);
 
-    return [messages];
+    return [state.ids.map((id) => state.messagesById[id])];
 }
