3f3a45e2e180664ccde20aac745e89b6a6e467ee
Koen Klaren <koen.klaren@gmail.com>
2019-02-10 15:20:13 +0100

    Add shoutbox footer

--- intranet-client/src/panels/shoutbox/ShoutboxPanel.tsx
+++ intranet-client/src/panels/shoutbox/ShoutboxPanel.tsx
@@ -4,12 +4,13 @@ import { useRef } from 'react';
 import usePrevious from '../../helpers/usePrevious';
 import { useScrollBottom } from '../../helpers/useScrollBottom';
 import Panel, { PanelProps } from '../Panel';
+import PanelGroup from '../PanelGroup';
 import { Shoutbox } from './Shoutbox';
 import useShoutbox from './useShoutbox';
 
 export type ShoutboxPanelProps = PanelProps;
 
-export default function ShoutboxPanel({ ...panelProps }: ShoutboxPanelProps) {
+export default function ShoutboxPanel(panelGroupProps: ShoutboxPanelProps) {
     const [messages] = useShoutbox();
     const prevMessageCount = usePrevious(messages.length);
 
@@ -17,8 +18,16 @@ export default function ShoutboxPanel({ ...panelProps }: ShoutboxPanelProps) {
     const onPanelScroll = useScrollBottom(panelRef, prevMessageCount !== 0);
 
     return (
-        <Panel {...panelProps} ref={panelRef} onScroll={onPanelScroll}>
-            <Shoutbox messages={messages} />
-        </Panel>
+        <PanelGroup direction="column" {...panelGroupProps}>
+            <Panel ref={panelRef} onScroll={onPanelScroll}>
+                <Shoutbox messages={messages} />
+            </Panel>
+            <Panel
+                flex="0 0 100px"
+                style={{ justifyContent: 'center', alignItems: 'center' }}
+            >
+                gepwnage.lan/shoutbox
+            </Panel>
+        </PanelGroup>
     );
 }
