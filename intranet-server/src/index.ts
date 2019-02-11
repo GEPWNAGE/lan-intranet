5c1af3f00871de185ab0f407533db7c18e2526db
Pieter Kokx <pieter@kokx.nl>
2019-02-12 00:53:54 +0100

    Added initial persistence

--- intranet-server/src/index.ts
+++ intranet-server/src/index.ts
@@ -8,14 +8,15 @@ const app = express();
 const server = new Server(app);
 const io = socketIo(server);
 
-let db = new sqlite3.Database('database.sqlite');
-
+// first initialize the database
+const db = new sqlite3.Database('database.sqlite');
 db.serialize(() => {
     // create shoutbox table
     db.run("CREATE TABLE IF NOT EXISTS shoutbox" +
         "(id integer not null primary key autoincrement, " +
-        "sender varchar not null, " +
-        "message text not null)");
+        "username varchar not null, " +
+        "body text not null, " +
+        "sent_at varchar not null)");
 });
 
 
@@ -37,23 +38,56 @@ if (true) {
 }
 
 app.get('/api/shoutbox', (req, res) => {
-    // TODO: Add some kind of pagination, we don't need to send all messages
-    res.json({ messages });
+    // for now, we simply do not allow scroll back, just send the last 20 messages
+    const sql = "SELECT id, username, body, sent_at FROM shoutbox ORDER BY id ASC LIMIT 20";
+    db.all(sql, (err, rows) => {
+        if (err !== null) {
+            console.log(err);
+            return;
+        }
+
+        const messages = rows.map(row => ({
+            id: row.id,
+            username: row.username,
+            body: row.body,
+            time: row.sent_at
+        }));
+
+        res.json({ messages });
+    });
 });
 
 const shoutbox = io.of('/shoutbox');
 
 // Send mock messages
 const mockUsers = [casual.username, casual.username, casual.username];
-let prevMessageId = 0;
 const messages = [
     generateMessage({ time: new Date(Date.now() - 60 * 1000) }),
     generateMessage({ time: new Date(Date.now() - 100 * 1000) }),
 ];
 
+function sendMessage(username: string, body: string, time = new Date()) {
+    // save the message in the database
+    const sql = "INSERT INTO shoutbox (id, username, body, sent_at) VALUES (NULL, ?, ?, ?)";
+    db.run(sql, [username, body, time.toJSON()], function(err) {
+        if (err !== null) {
+            console.log("SQLite error!", err);
+            return;
+        }
+        // emit the message to the shoutbox
+        const message = {
+            id: this.lastID,
+            username,
+            body,
+            time
+        };
+        shoutbox.emit('shoutbox message', message);
+    });
+
+}
+
 function generateMessage({ time = new Date() } = {}) {
     return {
-        id: prevMessageId++,
         username: casual.random_element(mockUsers),
         body: casual.sentence,
         time,
@@ -62,8 +96,8 @@ function generateMessage({ time = new Date() } = {}) {
 
 function emitMessage() {
     const message = generateMessage();
-    messages.push(message);
-    shoutbox.emit('shoutbox message', message);
+
+    sendMessage(message.username, message.body);
 
     setTimeout(emitMessage, casual.integer(2000, 60000));
 }
