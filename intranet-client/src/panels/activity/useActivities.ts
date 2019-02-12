fe3f4133c55ebe18b85c53af195d75f6539ed5c0
Koen Klaren <koen.klaren@gmail.com>
2019-02-12 19:30:10 +0100

    Update activities by re-fetching every 10 minutes

--- intranet-client/src/panels/activity/useActivities.ts
+++ intranet-client/src/panels/activity/useActivities.ts
@@ -6,8 +6,6 @@ export interface Activity {
     id: number;
     title: string;
     details: string;
-    canSubscribe: boolean;
-    startDate: Date;
 }
 
 async function handleResponse(res: Response): Promise<Activity[]> {
@@ -16,46 +14,20 @@ async function handleResponse(res: Response): Promise<Activity[]> {
         id: activity.id,
         title: activity.title,
         details: activity.details,
-        canSubscribe: activity.can_subscribe === 1,
-        startDate: new Date(Date.parse(activity.starts_at)),
     }));
 }
-
-function filterActivities(activities: Activity[]): Activity[] {
-    // Threshold date, one hour in the past to also include the current activity
-    const threshDate = new Date(new Date().getTime() - 60 * 60 * 1000);
-
-    return activities
-        .filter((act) => act.startDate.getTime() > threshDate.getTime())
-        .sort((a, b) => a.startDate.getTime() - b.startDate.getTime());
-}
-
 export default function useActivities(): Activity[] {
-    const allActivities = useFetch<Activity[]>(
-        'http://localhost:3030/api/activities',
+    const [version, setVersion] = useState(0);
+    const activities = useFetch<Activity[]>(
+        { version, url: 'http://localhost:3030/api/activities' },
         [],
         handleResponse,
     );
 
-    const [activities, setActivities] = useState<Activity[]>(() =>
-        filterActivities(allActivities),
-    );
-
-    // Remove finished activities
-    const updateFilteredActivities = useCallback(() => {
-        setActivities((activities) => {
-            const filtered = filterActivities(allActivities);
-            return filtered.length === activities.length
-                ? activities
-                : filtered;
-        });
-    }, [allActivities]);
-
-    // Update the filtered activities
-    // 1. when the array of all activities changes
-    // 2. every 10 minutes (to discard activities that finished while the beamer was running)
-    useEffect(updateFilteredActivities, [allActivities]);
-    useInterval(updateFilteredActivities, 10 * 60 * 1000);
+    // Request the activities again every 10 minutes
+    useInterval(() => {
+        setVersion((version) => version + 1);
+    }, 10 * 60 * 1000);
 
     return activities;
 }
