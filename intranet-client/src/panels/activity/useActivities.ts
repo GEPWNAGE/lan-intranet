d0e0fbc0ed965a2adacfec86c89c6a5ca42003f7
Koen Klaren <koen.klaren@gmail.com>
2019-02-12 19:37:26 +0100

    Cleanup imports

--- intranet-client/src/panels/activity/useActivities.ts
+++ intranet-client/src/panels/activity/useActivities.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useState } from 'react';
 import useFetch from '../../helpers/useFetch';
 import { useInterval } from '../../helpers/useInterval';
 
