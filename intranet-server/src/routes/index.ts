5b7a4eb2597033375f07fd14226cc2044198fb10
Koen Klaren <koen.klaren@gmail.com>
2019-02-12 22:27:35 +0100

    Render home page of the intranet

--- intranet-server/src/routes/index.ts
+++ intranet-server/src/routes/index.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.render('website/index');
 });
 
 router.get('/beamer', (req, res) => {
