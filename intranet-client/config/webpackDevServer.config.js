c9cb59ac91ff50af4bae9635825a8460159f9f24
Koen Klaren <koen.klaren@gmail.com>
2019-02-13 23:02:04 +0100

    Add CORS headers in dev server

--- intranet-client/config/webpackDevServer.config.js
+++ intranet-client/config/webpackDevServer.config.js
@@ -13,6 +13,11 @@ const host = process.env.HOST || '0.0.0.0';
 module.exports = function(proxy, allowedHost) {
   return {
     inline: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+    },
+
 
     // WebpackDevServer 2.4.3 introduced a security fix that prevents remote
     // websites from potentially accessing local content through DNS rebinding:
