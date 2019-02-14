2b3881a9a11609e19c6188b3f74602455fd7d199
Pieter Kokx <pieter@kokx.nl>
2019-02-14 16:12:40 +0100

    Added activity subscription

--- intranet-server/src/routes/index.ts
+++ intranet-server/src/routes/index.ts
@@ -80,6 +80,54 @@ router.get('/activity/:activityId([0-9]+)', (req, res) => {
     });
 });
 
+router.post('/activity/:activityId([0-9]+)/subscribe', async (req, res) => {
+    const activityId = parseInt(req.params.activityId);
+
+    const sql = 'SELECT id, title, details, can_subscribe FROM activities WHERE id = ?';
+    db.get(sql, [activityId], async (err, activity) => {
+        if (err !== null || activity === undefined) {
+            console.log(err);
+            res.render('website/error');
+            return;
+        }
+
+        if (activity.can_subscribe == 1) {
+        	try {
+        	    await dbRun('INSERT INTO subscriptions VALUES (NULL, ?, ?)',[
+        	        activityId,
+                    res.locals.hostname
+                ]);
+            } catch (err) {
+        	    console.log(err);
+                res.render('website/error');
+                return;
+            }
+        }
+
+        const sql =
+            'SELECT s.id, s.hostname, n.nick FROM subscriptions AS s ' +
+            'LEFT JOIN nicknames AS n ON (s.hostname = n.hostname)' +
+            'WHERE s.activity_id = ?';
+        db.all(sql, [activityId], (err, rows) => {
+            if (err !== null) {
+                console.log(err);
+                res.render('website/error');
+                return;
+            }
+
+            activity.subscriptions = rows.map((s) => ({
+                username:
+                    s.nick === null
+                        ? s.hostname
+                        : s.nick + ' [' + s.hostname + ']',
+                ...s,
+            }));
+
+            res.render('website/activity', { activity });
+        });
+    });
+});
+
 router.get('/change-nickname', (req, res) => {
 	res.render('website/change-nickname', {
 	    hostname: res.locals.hostname,
