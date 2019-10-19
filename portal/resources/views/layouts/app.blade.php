b893db8f0e17f06fc0fecc97c8240083bb0fd5ea
Pieter Kokx <pieter@kokx.nl>
2019-10-20 00:02:13 +0200

    Improved admin views more

--- portal/resources/views/layouts/app.blade.php
+++ portal/resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     <div id="admin-app">
         <nav class="navbar navbar-expand-md navbar-light">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/admin') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
