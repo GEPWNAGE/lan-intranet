91a26d0b54e8b5101811d175d7642a3a0501f8af
Pieter Kokx <pieter@kokx.nl>
2019-10-20 01:15:05 +0200

    Added participant model

---
+++ portal/app/Participant.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property string $name
+ */
+class Participant extends Model
+{
+    public $timestamps = false;
+}
