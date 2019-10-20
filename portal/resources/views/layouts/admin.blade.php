f74b07bc74d7ae41f8450ea31bedcc227a185465
Pieter Kokx <pieter@kokx.nl>
2019-10-20 22:51:21 +0200

    Added link to controller

--- portal/resources/views/layouts/admin.blade.php
+++ portal/resources/views/layouts/admin.blade.php
@@ -15,7 +15,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
 </head>
-<body class="admin">
+<body>
     <div id="admin-app">
         <nav class="navbar navbar-expand-md navbar-light">
             <div class="container">
@@ -44,9 +44,14 @@
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                         @else
+                            <li class="nav-item">
+                                <a href="{{env('UNIFI_URL', 'https://localhost:8443')}}" class="nav-link">
+                                    Controller
+                                </a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ Auth::user()->email }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
