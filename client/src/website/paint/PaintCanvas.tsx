41feb210105ac72a182cd98c7b877964006d3869
Pieter Kokx <pieter@kokx.nl>
2020-02-23 15:40:20 +0100

    Added simple colorpicker

--- client/src/website/paint/PaintCanvas.tsx
+++ client/src/website/paint/PaintCanvas.tsx
@@ -1,5 +1,18 @@
 import * as React from 'react';
-import { useEffect, useRef } from 'react';
+import { useEffect, useState, useRef } from 'react';
+
+interface ColorPickerProps {
+    color: string;
+    changeColor: (color: string) => void;
+};
+
+function ColorPicker(props: ColorPickerProps) {
+    return (
+        <div className="form-group">
+            <input type="string" value={props.color} onChange={e => props.changeColor(e.target.value)}/>
+        </div>
+    );
+}
 
 export interface PaintCanvasProps {
     size: number;
@@ -7,6 +20,7 @@ export interface PaintCanvasProps {
 
 export default function PaintCanvas(props: PaintCanvasProps) {
     const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [color, setColor] = useState('#013370');
 
     useEffect(() => {
         const canvas = canvasRef.current as HTMLCanvasElement;
@@ -16,9 +30,25 @@ export default function PaintCanvas(props: PaintCanvasProps) {
             return;
         }
 
+        const pixelSize = props.size / 128;
         context.clearRect(0, 0, props.size, props.size);
 
-        const pixelSize = props.size / 128;
+        canvas.addEventListener('mousedown', e => {
+            const bounding = canvas.getBoundingClientRect();
+            const mouseLoc = {
+                x: e.clientX - bounding.left,
+                y: e.clientY - bounding.top
+            };
+
+            const pixelLoc = {
+                x: Math.floor(mouseLoc.x / pixelSize),
+                y: Math.floor(mouseLoc.y / pixelSize)
+            };
+
+            context.fillStyle = color;
+            context.fillRect(pixelSize*pixelLoc.x, pixelSize*pixelLoc.y, pixelSize, pixelSize);
+        });
+
         for (let x = 0; x < 256; x++) {
             for (let y = 0; y < 256; y++) {
                 if ((x + y) % 2 === 0) {
@@ -33,5 +63,14 @@ export default function PaintCanvas(props: PaintCanvasProps) {
 
     });
 
-    return <div><canvas ref={canvasRef} width={props.size} height={props.size}/></div>;
+    return (
+        <div className="row">
+            <div className="col">
+                <ColorPicker color={color} changeColor={setColor}/>
+            </div>
+            <div className="col-11">
+                <canvas ref={canvasRef} width={props.size} height={props.size}/>
+            </div>
+        </div>
+    );
 }
