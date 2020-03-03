fb05a60fcfc672444965201064ec264b34aa239e
Pieter Kokx <pieter@kokx.nl>
2020-03-04 00:13:31 +0100

    Improved separation of concerns

--- client/src/website/paint/PaintCanvas.tsx
+++ client/src/website/paint/PaintCanvas.tsx
@@ -5,25 +5,7 @@ import useSocket from '../../helpers/useSocket';
 interface ColorPickerProps {
     color: string;
     changeColor: (color: string) => void;
-};
-
-function ColorPicker(props: ColorPickerProps) {
-    return (
-        <div className="form-group">
-            <input type="string" value={props.color} onChange={e => props.changeColor(e.target.value)}/>
-        </div>
-    );
 }
-
-export interface PaintCanvasProps {
-    size: number;
-}
-
-interface PaintMessage {
-    x: number;
-    y: number;
-    color: string;
-};
 
 async function sendPixelChange(x: number, y: number, color: string) {
     await fetch('/paint/api/pixel', {
@@ -36,8 +18,79 @@ async function sendPixelChange(x: number, y: number, color: string) {
     });
 }
 
-export default function PaintCanvas(props: PaintCanvasProps) {
+function ColorPicker(props: ColorPickerProps) {
+    return (
+        <div className="form-group">
+            <input type="string" value={props.color} onChange={e => props.changeColor(e.target.value)}/>
+        </div>
+    );
+}
+
+interface RenderCanvasProps {
+    onClick: (x: number, y: number) => void;
+    size: number;
+    grid: string[][];
+}
+
+function RenderCanvas(props: RenderCanvasProps) {
     const canvasRef = useRef<HTMLCanvasElement>(null);
+    const pixelSize = props.size / 128;
+
+    // draw the grid, when the canvas has been mounted
+    useEffect(() => {
+        const canvas = canvasRef.current as HTMLCanvasElement;
+        const context = canvas.getContext('2d');
+
+        if (context === null) {
+            return;
+        }
+
+        context.clearRect(0, 0, props.size, props.size);
+
+        for (let y = 0; y < 128; y++) {
+            for (let x = 0; x < 128; x++) {
+                context.fillStyle = props.grid[y][x];
+                context.fillRect(pixelSize*x, pixelSize*y, pixelSize, pixelSize);
+            }
+        }
+    });
+
+    console.log("not effect");
+
+    useEffect(() => {
+        console.log("Effect");
+    });
+
+    const handleClick = (e: any) => {
+        const canvas = canvasRef.current as HTMLCanvasElement;
+
+        const bounding = canvas.getBoundingClientRect();
+        const mousex = e.clientX - bounding.left - 1;
+        const mousey = e.clientY - bounding.top - 1;
+
+        const x = Math.floor(mousex / pixelSize);
+        const y = Math.floor(mousey / pixelSize);
+
+        props.onClick(x, y);
+    };
+
+    return <canvas ref={canvasRef}
+                   onClick={handleClick}
+                   width={props.size}
+                   height={props.size}/>;
+}
+
+export interface PaintCanvasProps {
+    size: number;
+}
+
+interface PaintMessage {
+    x: number;
+    y: number;
+    color: string;
+}
+
+export default function PaintCanvas(props: PaintCanvasProps) {
     const [color, setColor] = useState('#013370');
     const [grid, setGrid] = useState(() => {
         let grid = [];
@@ -55,12 +108,19 @@ export default function PaintCanvas(props: PaintCanvasProps) {
         return grid;
     });
     const [gridLoaded, setGridLoaded] = useState(false);
+    // to force rerendering
+    const [number, setNumber] = useState(0);
 
     const socket = useSocket();
 
     useEffect(() => {
         function callback(msg: PaintMessage) {
-            paintGrid(msg.x, msg.y, msg.color);
+            let newGrid = grid;
+            newGrid[msg.y][msg.x] = msg.color;
+            console.log('Setgrid');
+            setGrid(newGrid);
+            // force rerender
+            setNumber(number + 1);
         }
 
         if (socket !== null) {
@@ -80,58 +140,9 @@ export default function PaintCanvas(props: PaintCanvasProps) {
             .then(body => setGrid(body));
     }, [gridLoaded]);
 
-    const pixelSize = props.size / 128;
-
-    function paintGrid(x: number, y: number, color: string) {
-        const canvas = canvasRef.current as HTMLCanvasElement;
-        const context = canvas.getContext('2d');
-
-        if (context === null) {
-            return;
-        }
-
-        context.fillStyle = color;
-        context.fillRect(pixelSize*x, pixelSize*y, pixelSize, pixelSize);
-
-        const newGrid = grid;
-        newGrid[y][x] = color;
-        setGrid(newGrid);
-    }
-
-    function handleCanvasClick(e: any) {
-        const canvas = canvasRef.current as HTMLCanvasElement;
-
-        const bounding = canvas.getBoundingClientRect();
-        const mouseLoc = {
-            x: e.clientX - bounding.left - 1,
-            y: e.clientY - bounding.top - 1
-        };
-
-        const pixelLoc = {
-            x: Math.floor(mouseLoc.x / pixelSize),
-            y: Math.floor(mouseLoc.y / pixelSize)
-        };
-
-        sendPixelChange(pixelLoc.x, pixelLoc.y, color);
+    function handleCanvasClick(x: number, y: number) {
+        sendPixelChange(x, y, color);
     };
-
-    useEffect(() => {
-        const canvas = canvasRef.current as HTMLCanvasElement;
-        const context = canvas.getContext('2d');
-
-        if (context === null) {
-            return;
-        }
-
-        context.clearRect(0, 0, props.size, props.size);
-
-        for (let y = 0; y < 128; y++) {
-            for (let x = 0; x < 128; x++) {
-                context.fillStyle = grid[y][x];
-                context.fillRect(pixelSize*x, pixelSize*y, pixelSize, pixelSize);
-            }
-        }
-    });
 
     return (
         <div className="row">
@@ -139,7 +150,9 @@ export default function PaintCanvas(props: PaintCanvasProps) {
                 <ColorPicker color={color} changeColor={setColor}/>
             </div>
             <div className="col-11">
-                <canvas ref={canvasRef} onClick={handleCanvasClick} width={props.size} height={props.size}/>
+                <RenderCanvas onClick={handleCanvasClick}
+                              grid={grid}
+                              size={props.size}/>
             </div>
         </div>
     );
