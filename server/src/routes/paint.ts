da2c760fa3d419467c96558f8d8ce374533b2d42
Pieter Kokx <pieter@kokx.nl>
2020-03-03 01:33:56 +0100

    Added updating of the grid table

--- server/src/routes/paint.ts
+++ server/src/routes/paint.ts
@@ -10,8 +10,28 @@ import {
 const router = Router();
 
 router.post('/pixel', async (req, res) => {
+    // validation
+    if (typeof req.body.x !== 'number' || typeof req.body.y !== 'number') {
+        res.json({ success: false });
+        return;
+    }
+    if (req.body.x < 0 || req.body.x >= 128 || req.body.y < 0 || req.body.y >= 128) {
+        res.json({ success: false });
+        return;
+    }
+    if (!/^#[0-9a-fA-F]{6}$/.test(req.body.color)) {
+        res.json({ success: false });
+        return;
+    }
 
-    console.log(req.body);
+    await paintdbRun(
+        'UPDATE grid SET color=? WHERE x=? AND y=?',
+        [
+            req.body.color,
+            req.body.x,
+            req.body.y
+        ]
+    );
 
     res.json({
         success: true
